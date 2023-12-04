15801f029c1d418410f060e939c3862640743306
Rishabh patel <rishabhpatel0042@gmail.com>
2023-12-05 03:09:32 +0530

    sucess

--- website/views.py
+++ website/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
+def home(request):
+    #check to see if loggin in
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        #authenticate
+        user=authenticate(request,username=username,password=password)
+        if(user is not None):
+            login(request,user)
+            messages.success(request,"You have been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request,"There was an error login please try again...")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
 
-# Create your views here.
+    return render(request,'home.html',{})
+
+
+def logout_user(request):
+    pass
